b48a4217b6dade7193489791f166471c1df62c44
Martin Olsson <martin@minimum.se>
2015-12-10 23:50:32 +0100

    Add day 1 solution in scala

---
+++ scala/build.sbt
@@ -0,0 +1,9 @@
+name := "advent-of-code-2015"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.1.7" % "test"
+
+libraryDependencies += "junit" % "junit" % "4.11" % "test"
---
+++ scala/src/test/scala/Day01Tests.scala
@@ -0,0 +1,31 @@
+import org.scalatest.junit.JUnitSuite
+import org.junit.Test
+import org.junit.Assert._
+
+class Day01Tests extends JUnitSuite {
+
+  @Test
+  def verifyExamplesPart1Test() {
+    assertEquals(0, Day01.findFloor("(())"))
+    assertEquals(0, Day01.findFloor("()()"))
+    assertEquals(3, Day01.findFloor("((("))
+    assertEquals(3, Day01.findFloor("(()(()("))
+    assertEquals(3, Day01.findFloor("))((((("))
+    assertEquals(-1, Day01.findFloor("())"))
+    assertEquals(-1, Day01.findFloor("))("))
+    assertEquals(-3, Day01.findFloor(")))"))
+    assertEquals(-3, Day01.findFloor(")())())"))
+  }
+
+  @Test
+  def verifyExamplesPart2Test() {
+    assertEquals(1, Day01.findBasementIndex(")"))
+    assertEquals(5, Day01.findBasementIndex("()())"))
+  }
+
+  @Test
+  def verifyEdgeCases(): Unit = {
+    assertEquals(-1, Day01.findBasementIndex("("))
+  }
+
+  }
