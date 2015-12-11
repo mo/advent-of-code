6918c85e506fbde2d0f6a791e502ce219df6bdaa
Martin Olsson <martin@minimum.se>
2015-12-11 23:44:44 +0100

    Add scala solution for day 2

---
+++ scala/src/main/scala/Day02.scala
@@ -0,0 +1,35 @@
+object Day02 {
+
+  def parseDimensions(giftDimensionsStr: String) : (Int, Int, Int) = {
+    giftDimensionsStr.split("x").map(_.toInt) match {
+      case Array(l, w, h) => (l, w, h)
+    }
+  }
+
+  def calcNeededPaper(giftDimensions: (Int, Int, Int)): Int = {
+    val (l, w, h) = giftDimensions
+    val sideAreas = Seq(l*w, w*h, h*l)
+    val slack = sideAreas.min
+    sideAreas.map(_*2).sum + slack
+  }
+
+  def calcNeededRibbon(giftDimensions: (Int, Int, Int)): Int = {
+    val (l, w, h) = giftDimensions
+    Seq(l, w, h).sorted.take(2).map(_*2).sum + l*w*h
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val inputFile = io.Source.fromFile("data/day02.txt")
+    var totalPaperNeeded = 0
+    var totalRibbonNeeded = 0
+    for (giftDimensionLine <- inputFile.getLines()) {
+      val giftDimensions = parseDimensions(giftDimensionLine)
+      totalPaperNeeded += calcNeededPaper(giftDimensions)
+      totalRibbonNeeded += calcNeededRibbon(giftDimensions)
+    }
+    println("total paper needed: " + totalPaperNeeded + " square feet")
+    println("total ribbon needed: " + totalRibbonNeeded + " feet")
+  }
+
+}
---
+++ scala/src/test/scala/Day02Tests.scala
@@ -0,0 +1,15 @@
+import org.junit.Test
+import org.junit.Assert._
+
+class Day02Tests {
+
+  @Test
+  def verifyExamplesPart1(): Unit = {
+    assertEquals(58, Day02.calcNeededPaper(Day02.parseDimensions("2x3x4")))
+    assertEquals(43, Day02.calcNeededPaper(Day02.parseDimensions("1x1x10")))
+    assertEquals(34, Day02.calcNeededRibbon(Day02.parseDimensions("2x3x4")));
+    assertEquals(14, Day02.calcNeededRibbon(Day02.parseDimensions("1x1x10")));
+
+  }
+
+}
