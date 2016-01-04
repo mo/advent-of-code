ecf3089aeb80f506adb452a13e466869906688f9
Martin Olsson <martin@minimum.se>
2016-01-05 00:40:47 +0100

    Clean up day 1

--- scala/src/main/scala/Day01.scala
+++ scala/src/main/scala/Day01.scala
@@ -1,43 +1,28 @@
 object Day01 {
 
   def findFloor(input: String): Int = {
-    var floor = 0
-    for (ch <- input) {
-      ch match {
-        case '(' => floor += 1
-        case ')' => floor -= 1
-      }
-    }
-    floor
+    input.map {
+      case '(' => 1
+      case ')' => -1
+    }.sum
   }
 
   def findBasementIndex(input: String): Int = {
-    var index = 0
-    var floor = 0
-    for (ch <- input) {
-      index += 1
-      ch match {
-        case '(' => floor += 1
-        case ')' => floor -= 1
-      }
-      if (floor == -1) {
-        return index
-      }
-    }
-    -1
+    input.map {
+      case '(' => 1
+      case ')' => -1
+    }.scanLeft(0)(_+_).indexOf(-1)
   }
 
   def solve(): (Int, Int) = {
     val directions = DataFolder.openFile("day01.txt").mkString
-    val finalFloor = findFloor(directions)
-    val firstBasementIndex = findBasementIndex(directions)
-    (finalFloor, firstBasementIndex)
+    (findFloor(directions), findBasementIndex(directions))
   }
 
   def main(args: Array[String]): Unit = {
     val (finalFloor, firstBasementIndex) = solve()
-    println("final floor: " + finalFloor)
-    println("first entered basement at index: " + firstBasementIndex)
+    println(s"final floor: $finalFloor")
+    println(s"first entered basement at index: $firstBasementIndex")
   }
 
 }
