707f8e5d3ff4a515b00a778a853f5435f881d4c8
Martin Olsson <martin@minimum.se>
2016-01-05 17:55:39 +0100

    Use all CPU cores when hashing advent coins for day 4
    This just makes it run in ~80% of the single core time because the
    JVM eats so much time for JIT. If you run solve() a few times before
    you measure, then the parallel version takes ~54% of the single core
    speed instead. Better but Amdahl is still nowhere to be seen (I'm
    measuring this on a quad core machine). I like how this particular
    way of parallelizing causes minimal disruption to code readability
    though.

--- scala/src/main/scala/Day04.scala
+++ scala/src/main/scala/Day04.scala
@@ -5,11 +5,13 @@ object Day04 {
   def md5(s: String) = MessageDigest.getInstance("MD5").digest(s.getBytes)
 
   def calcAdventCoinNumber(zeros: Int, key: String) : Int = {
-    Iterator.from(0).indexWhere {
+    require(zeros >= 2)
+    val BATCH_SIZE = 20000
+    Iterator.from(0).grouped(BATCH_SIZE).flatMap(_.par.filter {
       i =>
         val hash = md5(key + i)
         hash(0) == 0 && hash.slice(0, 1 + zeros / 2).flatMap(byte => Seq(byte >> 4, byte & 0xF)).slice(0, zeros).forall(_ == 0)
-    }
+    }).next()
   }
 
   def solve(): (Int, Int) = {
